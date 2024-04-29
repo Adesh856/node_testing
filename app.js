6796c8e5db28e81c30ea9322668bff220e6fc771
Adesh856 <adeshtayde6@gmail.com>
2024-04-29 17:29:11 +0530

    Added git actions

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express")
 const { config } = require("./config")
 const { connection } = require("./db/database")
 const logger = require("./helper/logger")
+const { execSync } = require('child_process');
 const app = express()
-
-
+const ipAddress = execSync('curl -s ip.me.uk').toString().trim();
 app.use(express.json())
 
 
@@ -17,7 +17,7 @@ app.get("/",(req,res)=>{
 app.listen(config.database.port,async()=>{
       try {
         await connection
-        logger.info("Database is connected")
+        logger.info("Database is connected"+` http://${ipAddress}:${config.database.port}`)
       } catch (error) {
         console.log({error})
         logger.info("Database is not connected")
